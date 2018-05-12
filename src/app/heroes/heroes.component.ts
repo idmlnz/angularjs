e5954fa0333c8c1604efb9b50d175f44960c5c16
Ismelito Malonzo <idmlnz@yahoo.com>
2018-05-12 15:49:01 -0700

    Added HTTP changes.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+
  
 @Component({
   selector: 'app-heroes',
@@ -22,7 +23,7 @@ export class HeroesComponent implements OnInit {
   ngOnInit() { 
     this.getHeroes();
   }
- 
+
   /*
   getHeroes(): void {
     this.heroes = this.heroService.getHeroes();
@@ -47,4 +48,20 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
   */
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
+
 }
